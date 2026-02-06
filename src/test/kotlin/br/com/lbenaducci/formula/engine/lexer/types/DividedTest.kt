505ca688bd3e008f854ed828fd78c581c5fde4c9
Leo Benaducci <leo13benaducci@gmail.com>
2026-02-06 15:05:14 -0300

    Feat[Lexer]: implementa suporte ao token de divisão

---
+++ src/test/kotlin/br/com/lbenaducci/formula/engine/lexer/types/DividedTest.kt
@@ -0,0 +1,51 @@
+package br.com.lbenaducci.formula.engine.lexer.types
+
+import br.com.lbenaducci.formula.engine.lexer.Carriage
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Nested
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import kotlin.test.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertIs
+import kotlin.test.assertTrue
+
+class DividedTest {
+    @Nested
+    inner class Alias {
+        @Test
+        fun `then return alias`() {
+            assertEquals("DIVIDED BY", Divided.alias)
+        }
+    }
+
+    @Nested
+    inner class Matches {
+        @Test
+        fun `given slash char, then return true`() {
+            assertTrue { Divided.matches('/') }
+        }
+
+        @Test
+        fun `given other char, then return false`() {
+            assertFalse { Divided.matches('a') }
+        }
+    }
+
+    @Nested
+    inner class Tokenize {
+        @Test
+        fun `given lexer, then return token`() {
+            val carriage = mock<Carriage> {
+                on { peek() } doReturn '/'
+                on { position } doReturn 0
+            }
+
+            val token = Divided.tokenize(carriage)
+
+            assertIs<Divided>(token.type)
+            assertEquals("/", token.lexeme)
+            assertEquals(0, token.position)
+        }
+    }
+}
